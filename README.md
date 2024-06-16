5eb5357c7f4037fbe2bec4bb6b29e4733dde26e1
unnxt30 <officialunnat30@gmail.com>
2024-06-16 13:38:20 +0530

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # GO-Chat
-A Chat server and cliend implemented in GO
+
+A real-time chat server and client written in GO.
+
+
+
+### Techniques Utilised:
+- `net` package of GO Standard Library
+- `channels` & `goroutines` to maintain concurrency in communication between multiple clients and server
+- `tcp` network protocol
+
+
+### How to Run
+- Run `go run ./server` in a terminal window
+- Run `go run ./client` in seperate terminal windows, to create multiple users.
+- Communicate between the two clients by sending messages.
+- Kill the server after completion `ctrl-c`
+
+### Pre-requisites
+- [Install](https://go.dev/doc/install) GO  
--- client/main.go
+++ client/main.go
@@ -8,17 +8,36 @@ import (
 	"strings"
 )
 
-func GetResponse(conn net.Conn) {
+func GetResponse(conn net.Conn, done chan<- bool) {
 	messageScanner := bufio.NewScanner(conn)
-	messageScanner.Scan()
+	for messageScanner.Scan() {
+		fmt.Println(messageScanner.Text())
+	}
 
-	fmt.Println(messageScanner.Text())
+	if err := messageScanner.Err(); err != nil {
+		fmt.Printf("Server disconnected: %v\n", err)
+		done <- true
+		return
+	}
+}
+
+func ReadInput(reader *bufio.Reader, inputChan chan<- string) {
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Error reading input: %v\n", err)
+			return
+		}
+
+		inputChan <- message
+	}
 }
 
 func main() {
 	conn, err := net.Dial("tcp", ":7007")
 	if err != nil {
-		panic(err)
+		fmt.Printf("Failed to connect to server: %v\n", err)
+		return
 	}
 	defer conn.Close()
 
@@ -27,26 +46,32 @@ func main() {
 	name, _ := reader.ReadString('\n')
 	_, err = fmt.Fprintf(conn, "%s", name)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Failed to send name: %v\n", err)
+		return
 	}
+
+	done := make(chan bool)
+	inputChan := make(chan string)
+	go GetResponse(conn, done)
+	go ReadInput(reader, inputChan)
+
 	for {
-		message, err := reader.ReadString('\n')
-		if err != nil {
-			panic(err)
-			conn.Close()
-		}
+		select {
+		case <-done:
+			fmt.Println("Server disconnected, exiting...")
+			return
+		case message := <-inputChan:
+			if strings.ToLower(message) == "quit\n" || strings.ToLower(message) == "exit\n" {
+				fmt.Printf("%s signing off :)\n", name)
+				conn.Close()
+				return
+			}
 
-		if strings.ToLower(message) == "quit\n" || strings.ToLower(message) == "exit\n" {
-			fmt.Printf("%s signing off :)\n", name)
-			break
+			_, err = fmt.Fprintf(conn, message)
+			if err != nil {
+				fmt.Printf("Failed to send message: %v\n", err)
+				return
+			}
 		}
-
-		_, err = fmt.Fprintf(conn, message)
-		if err != nil {
-			panic(err)
-		}
-
-		GetResponse(conn)
 	}
-
 }
